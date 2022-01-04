f9c320eebdbc1e9d9c49781c6119e1dc4ae07c9f
Chandrakesh Ram <79987328+chandrakeshram@users.noreply.github.com>
2022-01-04 22:31:17 +0530

    Add files via upload

---
+++ ClassObject.java
@@ -0,0 +1,23 @@
+class Student{      //Here class has been created of name Student 
+    int roll_no;  // roll_no , name and marks are the data members of class Student
+    String name;  //display() is the data function of class student
+    int marks;
+    Student(int roll, String name,int marks){  //Here we have created a Constructor
+        this.roll_no=roll;    //we have initialized the value of data members
+        this.name=name;
+        this.marks=marks;
+
+
+    }
+    void display(){       //display function is used to display all the details of student
+        System.out.println("Roll no : "+roll_no);
+        System.out.println("Name :"+name);
+        System.out.println("Marks :"+marks);
+    }
+}
+public class ClassObject{
+    public static void main(String []args ){
+        Student s1=new Student(25058,"Chandrakesh",78);//here we have created an object with help of class Student
+        s1.display();       //here we have accessed the data function of the class Student with help of object
+    }
+}
