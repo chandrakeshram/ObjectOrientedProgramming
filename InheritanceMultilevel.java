94be6533ded8b096619a89faf43f9f91a78b93c5
Chandrakesh Ram <79987328+chandrakeshram@users.noreply.github.com>
2022-01-16 20:40:16 +0530

    Just added explaination in the code

--- InheritanceMultilevel.java
+++ InheritanceMultilevel.java
@@ -1,20 +1,21 @@
-class Book{
+class Book{          //here we have created class Book 
     int price;
     String name;
     String author;
-    void display(){
+    void display(){     //this is void method called display()
         System.out.println("Name of Book : "+this.name);
         System.out.println("Author of Book : "+this.author);
         System.out.println("Price of Book : "+this.price);
     }
 }
-class Comics extends Book{
+class Comics extends Book{    //here class Comics inherits class Book 
     void purpose(String s){
         System.out.println("Purpose of the Comics is make  :"+s);
     }
 }
-class Tom_Jerry extends Comics{
-
+class Tom_Jerry extends Comics{    //here class Tom_Jerry inherits class Comics 
+                                   //so class Book is inherited by class Tom_Jerry indirectly
+                                   //this is example of multilevel inheritance 
 }
 public class InheritanceMultilevel{
     public static void main(String []args){
@@ -26,3 +27,14 @@ public class InheritanceMultilevel{
         b1.purpose("Laugh");
     }
 }
+
+
+
+//NOTE:
+/*                           BOOK
+                               |
+                            COMIC_BOOK
+                               |
+                            TOM_JERRY
+                   
+*/
